9c5e093d45bffd26cf3e917116aab9a1bb99b8d7
panghaidar <ksatria_xx@gmail.com>
2018-11-24 19:20:18 +0700

    upload

---
+++ src/assets/js/setting.js
@@ -0,0 +1,10 @@
+var wordpress_url = "https://www.marketplace.kholis.net";
+var onesignal_app_id = "52427d48-d40b-4df7-a091-14048475ea4f";
+var google_web_api_key = "486944611348-ubf0mlrhtabosimclvohndc6r39qfo0g.apps.googleusercontent.com";
+var wordpress_per_page = 10;
+var post_per_page = 4;
+var request_timeout = 15000; // milliseconds 
+var notice_timeout = 2000; // milliseconds
+var slide_time = 3000; // milliseconds
+var hide_sale = true;
+var hide_available_stock = true;
